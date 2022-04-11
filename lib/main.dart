549d0be40792965d0481f65840112f036fc9783b
Jannatul13064 <akzzislam19194@gmail.com>
2022-04-11 23:02:21 +0600

    Five

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class MyApp extends StatelessWidget {
       ],
       child: Consumer<Auth>(
         builder: (ctx, auth, _) => MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'MyShop',
           theme: ThemeData(
             fontFamily: 'Lato',
